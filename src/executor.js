500cc91ff597c54b757cd7c087cfb310c0999bf7
tinder-ktarasov <tinder-ktarasov@users.noreply.github.com>
2017-06-08 15:39:22 +0300

    Customization to run tests via SSH on CircleCI
    - Option to specify first worker node
    - Option to specify exported env variables

--- src/executor.js
+++ src/executor.js
@@ -1,4 +1,7 @@
-import { fork } from "child_process";
+import { spawn, execFileSync } from "child_process";
+import logger from "testarmada-logger";
+import path from 'path';
+import settings from "./settings";
 
 export default {
   /*eslint-disable no-unused-vars*/
@@ -24,13 +27,48 @@ export default {
   },
 
   execute: (testRun, options, mocks = null) => {
-    let ifork = fork;
+    logger.prefix = "CircleCI Executor";
+
+    let ispawnSync = execFileSync;
+    let ispawn = spawn;
 
     if (mocks && mocks.fork) {
-      ifork = mocks.fork;
+      ispawnSync = mocks.fork;
+      ispawn = mocks.fork;
     }
 
-    return ifork(testRun.getCommand(), testRun.getArguments(), options);
+    const nodeIndex = Number(settings.config.firstNode) + Number(testRun.workerIndex) - 1; // workerIndex is one based
+    const fullPath = path.resolve(testRun.tempAssetPath);
+
+    const syncOptions = Object.assign({}, options, { stdio: ['pipe', 'pipe', 'pipe' ] });
+
+    const mkdirOutput = ispawnSync('ssh',
+      [`node${nodeIndex}`, 'mkdir', '-p', fullPath],
+      syncOptions);
+    logger.log(mkdirOutput);
+
+    const copyOutput = ispawnSync('rsync', [
+        '-rpt', '-z', '-vv', '--delete',
+        '-e', 'ssh',
+        fullPath, `node${nodeIndex}:"${fullPath}/.."`],
+      syncOptions);
+    logger.log(copyOutput);
+
+    let remoteArgs = [`node${nodeIndex}`, '--'];
+    const runPath = path.resolve('.');
+    remoteArgs = remoteArgs.concat(['cd', runPath, ';']);
+
+    settings.config.exportedEnvVars.forEach(env => {
+      const varName = env.toUpperCase();
+      const value = (typeof process.env[varName] === undefined) ? '' : process.env[varName];
+      remoteArgs = remoteArgs.concat(['export', `"${varName}"="${value}"`, ';']);
+    });
+
+    remoteArgs.push(testRun.getCommand());
+    remoteArgs.push( '"'+testRun.getArguments().join('" "') + '"' );
+    logger.log(remoteArgs);
+
+    return ispawn('ssh', remoteArgs, options);
   },
 
   summerizeTest: (magellanBuildId, testResult, callback) => {
--- src/help.js
+++ src/help.js
@@ -15,5 +15,15 @@ export default {
     "visible": true,
     "type": "function",
     "description": "List the available browsers configured."
+  },
+  "remote_first_node": {
+    "visible": true,
+    "type": "string",
+    "description": "Index of the first node reserved for tests execution in CircleCI."
+  },
+  "remote_exported_vars": {
+    "visible": true,
+    "type": "string",
+    "description": "Environment vars exported to a remote node before executing a test."
   }
 };
